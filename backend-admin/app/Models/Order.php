73251dcda45dd35f530687931544af906f8d16af
Jjin SoL <parkleydondon@yahoo.com>
2021-04-27 23:42:01 -0700

    Added Orders (migration,models,controller,resources)

---
+++ backend-admin/app/Models/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\OrderItem;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function orderItems() {
+        return $this->hasMany(OrderItem::class);
+    }
+}
---
+++ backend-admin/database/seeders/OrderSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Database\Seeder;
+use phpseclib3\Crypt\Random;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Order::factory(20)->create()
+        ->each(function(Order $order) {
+            OrderItem::factory(random_int(1,5))->create([
+                'order_id' => $order->id,
+            ]);
+        });
+    }
+}
