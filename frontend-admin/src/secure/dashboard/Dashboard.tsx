2594774c3f0ad2924bf3f3612f57d87c22c37791
Jjin SoL <parkleydondon@yahoo.com>
2021-05-29 12:00:35 -0700

    React: adding Chart on Dashboard

--- frontend-admin/src/secure/dashboard/Dashboard.tsx
+++ frontend-admin/src/secure/dashboard/Dashboard.tsx
@@ -1,23 +1,38 @@
-import React from "react"
+import React, { Component } from "react"
 import Wrapper from "../Wrapper"
+import c3 from "c3"
 
-const Dashboard = () => (
-  <Wrapper>
-    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-      <h1 className="h2">Dashboard</h1>
-      <div className="btn-toolbar mb-2 mb-md-0">
-        <div className="btn-group mr-2">
-          <button className="btn btn-sm btn-outline-secondary">Share</button>
-          <button className="btn btn-sm btn-outline-secondary">Export</button>
-        </div>
-        <button className="btn btn-sm btn-outline-secondary dropdown-toggle">
-          <span data-feather="calendar"></span>
-          This week
-        </button>
-      </div>
-    </div>
+class Dashboard extends Component {
+  componentDidMount() {
+    let chart = c3.generate({
+      bindto: "#chart",
+      data: {
+        x: "x",
+        columns: [["x"], ["sales"]],
+        types: {
+          Sales: "bar"
+        }
+      },
+      axis: {
+        x: {
+          type: "timeseries",
+          tick: {
+            format: "%Y-%m-%d"
+          }
+        }
+      }
+    })
+  }
 
-    <canvas className="my-4" id="myChart" width="900" height="380"></canvas>
-  </Wrapper>
-)
+  render() {
+    return (
+      <Wrapper>
+        <h2>Daily Sales</h2>
+
+        <div id="chart" />
+        {/* <canvas className="my-4" id="myChart" width="900" height="380"></canvas> */}
+      </Wrapper>
+    )
+  }
+}
 export default Dashboard
