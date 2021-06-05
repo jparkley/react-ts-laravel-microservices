79e0a4d83b50f586130aebbf81049ffdc03aa088
Jjin SoL <parkleydondon@yahoo.com>
2021-06-05 10:28:52 -0700

    React: added Sales Chart on Dashboard

--- frontend-admin/src/secure/dashboard/Dashboard.tsx
+++ frontend-admin/src/secure/dashboard/Dashboard.tsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react"
 import Wrapper from "../Wrapper"
 import c3 from "c3"
+import axios from "axios"
 
 class Dashboard extends Component {
-  componentDidMount() {
+  componentDidMount = async () => {
     let chart = c3.generate({
       bindto: "#chart",
       data: {
         x: "x",
-        columns: [["x"], ["sales"]],
+        columns: [
+          ["x", "2021-1-1", "2021-2-1"],
+          ["sales", 100, 200]
+        ],
         types: {
           Sales: "bar"
         }
@@ -22,6 +26,18 @@ class Dashboard extends Component {
         }
       }
     })
+
+    const response = await axios.get("chart")
+    const records: { date: string; sum: number }[] = response.data.data
+
+    chart.load({
+      columns: [
+        ["x", ...records.map(r => r.date)],
+        ["Sales", ...records.map(r => r.sum)]
+      ]
+    })
+
+    console.log(records)
   }
 
   render() {
